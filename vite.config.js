f417dc5fe1be4072119448e97ad5fe54f8e282f1
diegobejaranov <diegob2826@hotmail.com>
2026-03-16 15:17:36 -0500

    corrigiendo rutas

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/HTML-CSS-Exercise/'
+  base: './'
+  import { resolve } from 'path'
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        quienes: resolve(__dirname, 'quienes-somos.html'),
+        aviso: resolve(__dirname, 'aviso_legal.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contacto: resolve(__dirname, 'contacto.html'),
+        cursos: resolve(__dirname, 'cursos.html'),
+        login: resolve(__dirname, 'login.html'),
+        registro: resolve(__dirname, 'registro.html'), 
+      }
+    }
+  }
 })
