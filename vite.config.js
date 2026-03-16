133797f5c9c439310cf4c2ae4b2cfd6b1d9d985e
diegobejaranov <diegob2826@hotmail.com>
2026-03-16 15:25:57 -0500

    Reajustando directorios para Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: './'
-  import { resolve } from 'path'
+  base: './',
   build: {
     rollupOptions: {
       input: {
